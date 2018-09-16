bbc54faf63c6e9b510cba07ee68a02ed35c49bd1
Monica <monicajromero5683@hotmail.com>
2018-09-16 00:01:26 -0400

    Practice switch and added a folder with two files.

---
+++ htdocs/series/switch/index.php
@@ -0,0 +1,136 @@
+<?php
+
+$number = 1;
+
+switch ($number) {
+ case 1;
+      echo 'one';
+  break;
+  
+  case 2;
+       echo 'two';
+  break;
+  
+  case 3;
+         echo 'three';
+  break;
+}
+
+echo '</br>';
+
+$number = 2;
+
+switch ($number) {
+ case 1;
+      echo 'one';
+  break;
+  
+  case 2;
+       echo 'two';
+  break;
+  
+  case 3;
+         echo 'three';
+  break;
+}
+
+echo '</br>';
+
+$number = 3;
+
+switch ($number) {
+ case 1;
+      echo 'one';
+  break;
+  
+  case 2;
+       echo 'two';
+  break;
+  
+  case 3;
+         echo 'three';
+  break;
+}
+
+echo '</br>';
+
+$number = 4;
+
+switch ($number) {
+ case 1;
+      echo 'one';
+  break;
+  
+  case 2;
+       echo 'two';
+  break;
+  
+  case 3;
+         echo 'three';
+  break;
+  
+  default;
+          echo 'Number not found.';
+  break;
+
+}
+
+echo '</br>';
+
+$number = -10;
+
+switch ($number) {
+ case 1;
+      echo 'one';
+  break;
+  
+  case 2;
+       echo 'two';
+  break;
+  
+  case 3;
+         echo 'three';
+  break;
+  
+  default;
+          echo 'Number not found.';
+  break;
+
+}
+
+echo '</br>';
+
+$day = 'Monday';
+
+switch ($day) {
+    case 'Saturday';
+    case 'Sunday';
+         echo 'Tis a weekend';
+    break;
+    
+    default;
+            echo 'Tis not a weekend';
+    break;
+
+}
+
+echo '</br>';
+
+$day = 'Saturday';
+
+switch ($day) {
+    case 'Saturday';
+    case 'Sunday';
+         echo 'Tis a weekend';
+    break;
+    
+    default;
+            echo 'Tis not a weekend';
+    break;
+
+}
+
+
+?>
+
+
