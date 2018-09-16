a6bbfbf4615633344c064aa0cb2e411881ea5f8d
Monica <monicajromero5683@hotmail.com>
2018-09-16 16:36:06 -0400

    Learned die and exit functions - also made a folder called dieexit and two files in it.

---
+++ htdocs/series/dieexit/index.php
@@ -0,0 +1,13 @@
+<?php
+
+@mysqli_connect('localhost','host') or die('Could not connect to the database.');
+
+echo 'Connected!';
+
+echo '</br>';
+
+@mysqli_connect('localhostd','monica') or die('Could not connect to the database.');
+
+echo 'Connected!';
+
+?>
