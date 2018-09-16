7db938a2b65b6bf34dc1f3aad7c6cbb2d738586f
Monica <monicajromero5683@hotmail.com>
2018-09-15 23:16:39 -0400

    Had fun with this lessson. Also created a folder and two files.

---
+++ htdocs/series/whileloop/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$counter = 1;
+
+while ($counter<=10) {
+ echo 'Howdy!<br>';
+ $counter++;
+}
+
+echo '</br>';
+
+$counter = 1;
+
+while ($counter<=10) {
+ echo $counter.'Howdy!<br>';
+ $counter++;
+} 
+
+?>
+
+
