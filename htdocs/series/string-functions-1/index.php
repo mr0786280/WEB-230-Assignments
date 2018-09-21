f9a4af1e758e047cee6915bf5c5d2670549474a3
Monica <monicajromero5683@hotmail.com>
2018-09-21 19:24:07 -0400

    Practiced string and array.

---
+++ htdocs/series/string-functions-1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+ $string = 'This is an example string.';
+ $string_word_count = str_word_count($string, 2);
+
+print_r ($string_word_count);
+
+echo '</br>';
+
+ $string = 'This is an example string.';
+ $string_word_count = str_word_count($string, 1, '.');
+
+print_r ($string_word_count);
+?>
