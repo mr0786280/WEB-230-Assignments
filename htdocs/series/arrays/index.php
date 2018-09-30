48a70c05516d36c52a4983d44beaea84b83cf333
Monica <monicajromero5683@hotmail.com>
2018-09-30 16:16:42 -0400

    Added new folder called arrays and practiced arrays.

---
+++ htdocs/series/arrays/index.php
@@ -0,0 +1,41 @@
+  <?php
+  
+  $food = array('Pasta', 'Pizza', 'Salad');
+
+  echo $food [0];
+  
+  echo '</br>';
+  
+   $food = array('Pasta', 'Pizza', 'Salad');
+
+  echo $food [1];
+  
+  echo '</br>';
+  
+   $food = array('Pasta', 'Pizza', 'Salad');
+
+  echo $food [2];
+  
+  echo '</br>';
+  
+   $food = array('Pasta', 'Pizza', 'Salad');
+
+  print_r($food);
+  
+  echo '</br>';
+  
+   $food = array('Pasta', 'Pizza', 'Salad', 'Vegetable');
+
+  print_r ($food);
+
+  echo '</br>';
+  
+  $food = array('Pasta', 'Pizza', 'Salad', 'Vegetable');
+
+ $food[4] = 'Fruit';
+ 
+ print_r($food);
+
+  echo '</br>';
+
+  ?>
