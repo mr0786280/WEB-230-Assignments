f5d2cb5f581599707669436c6300a6826d43a3c0
Monica <monicajromero5683@hotmail.com>
2018-10-01 18:55:24 -0400

    Added a new folder and two new files.

---
+++ htdocs/series/associative-arrays/index.php
@@ -0,0 +1,27 @@
+  <?php
+  
+  $food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150, 'Vegetables'=>50);
+
+  print_r ($food);
+  
+  echo '</br>';
+  
+  $food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150, 'Vegetables'=>50);
+
+  echo $food ['Pasta'];
+  
+  echo '</br>';
+
+  $food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150, 'Vegetables'=>50);
+
+  echo $food ['Pizza'];
+  
+  echo '</br>';
+
+  $food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150, 'Vegetables'=>50);
+
+  echo $food ['Salad'];
+
+  echo '</br>';
+
+  ?>
