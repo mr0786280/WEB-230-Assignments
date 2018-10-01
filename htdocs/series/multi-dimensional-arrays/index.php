7c66e9ac05547f0803b5c56ee0cddb01569a7d0d
Monica <monicajromero5683@hotmail.com>
2018-10-01 19:59:24 -0400

    Added a new folder and practiced multi-dimensional arrays.

---
+++ htdocs/series/multi-dimensional-arrays/index.php
@@ -0,0 +1,48 @@
+  <?php
+
+  $food = array('Healthy' =>
+                          array ('Salad', 'Vegetables', 'Pasta'),
+                   'Unhealthy'=>
+                          array ('Pizza','Ice cream'));
+  
+  echo $food ['Healthy'][0];
+  
+  echo '</br>';
+
+    $food = array('Healthy' =>
+                          array ('Salad', 'Vegetables', 'Pasta'),
+                   'Unhealthy'=>
+                          array ('Pizza','Ice cream'));
+  
+  echo $food ['Healthy'][1];
+  
+  echo '</br>';
+  
+    $food = array('Healthy' =>
+                          array ('Salad', 'Vegetables', 'Pasta'),
+                   'Unhealthy'=>
+                          array ('Pizza','Ice cream'));
+  
+  echo $food ['Healthy'][2];
+  
+  echo '</br>';
+  
+    $food = array('Healthy' =>
+                          array ('Salad', 'Vegetables', 'Pasta'),
+                   'Unhealthy'=>
+                          array ('Pizza','Ice cream'));
+  
+  echo $food ['Unhealthy'][0];
+  
+  echo '</br>';
+  
+    $food = array('Healthy' =>
+                          array ('Salad', 'Vegetables', 'Pasta'),
+                   'Unhealthy'=>
+                          array ('Pizza','Ice cream'));
+  
+  echo $food ['Unhealthy'][1];
+
+  echo '</br>';
+
+  ?>
