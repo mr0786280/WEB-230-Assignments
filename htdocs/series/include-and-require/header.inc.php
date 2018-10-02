7bbeb68e2655c4c63926ac467b800ba2ece1bbd3
Monica <monicajromero5683@hotmail.com>
2018-10-01 22:18:19 -0400

    Added new folder and files, then practiced require and include functions.

---
+++ htdocs/series/include-and-require/header.inc.php
@@ -0,0 +1,8 @@
+<h1> My page </h1>
+
+<?php
+
+$var1 = 21;
+$var2 = 'String';
+
+?>
---
+++ htdocs/series/include-and-require/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'header.inc.php';
+//include 'header2.inc.php';
+//include 'header2.inc.php';
+
+echo 'Var2 is  '.$var2;
+
+
+?>
