4f718d0a15a52d9f672eb919dfc44d1c47119ea1
Monica <monicajromero5683@hotmail.com>
2018-10-01 22:48:49 -0400

    Practiced require_once and include_once and added a folder and four files.

---
+++ htdocs/series/include_once-and-require_once/header.inc.php
@@ -0,0 +1,8 @@
+<h1> My page </h1>
+
+<?php    
+
+$var1 = 'String';
+$var2 = '21';
+
+?>
---
+++ htdocs/series/include_once-and-require_once/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//include_once 'header.inc.php';
+include 'header.inc.php';
+require 'header.inc.php';
+
+echo 'var2 is  '.$var2;
+
+?>
