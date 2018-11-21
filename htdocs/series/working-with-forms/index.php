af43cf22b6e288456cd08f1147f998ee580383ce
Monica <monicajromero5683@hotmail.com>
2018-11-21 16:57:53 -0500

    Did working with forms.

---
+++ htdocs/series/working-with-forms/index.php
@@ -0,0 +1,20 @@
+<?php
+//01. Explain what the action attribute is and does.
+//A1: This attribute defines where data will be sent.
+
+
+//02. Explain what the method attribute is and does.
+//A2: This attribute defines how the data is sent.
+
+
+
+?>
+
+<form action="index.php" method="GET">
+
+      Day:  <br><input type="text" name="day"><br>
+      Date: <br><input type="text" name="date"><br>
+      Year: <br><input type="text" name="year"><br>
+      <br><input type="Submit" value="Submit">
+
+</form>
