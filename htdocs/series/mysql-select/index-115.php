7ca4e2c5a9405afe1e8ea3b70e3ab9a0bda3fb8e
Monica <monicajromero5683@hotmail.com>
2018-12-14 02:52:12 -0500

    Did mysqli_select.

---
+++ htdocs/series/mysql-select/index-115.php
@@ -0,0 +1,13 @@
+<?php
+
+     require 'connect.inc.php';
+     
+     $query = "SELECT food, calories FROM food ORDER BY 'id'";
+     
+     if ($query_run = mysqli_query($con, $query)) {
+       echo 'Query succeded.';
+     } else {
+       echo 'Query has failed.';
+     }
+     
+?>
---
+++ htdocs/series/mysql-select/index-119.php
@@ -0,0 +1,53 @@
+<?php
+
+    require 'connect.inc.php';
+
+?>
+
+
+  <form action="index-119.php" method="GET">
+
+    Choose a food type:
+    <select name="uh">
+
+      <option value="u">Unhealthy</option>
+      <option value="h">Healthy</option>
+
+    </select><br><br>
+
+    <input type="submit" value="Submit">
+
+  </form>
+
+
+<?php
+
+    if(isset($_GET['uh']) && !empty($_GET['uh'])){
+
+        $uh = strtolower($_GET['uh']);
+
+        if($uh=='u'||$uh=='h'){
+
+            $query = "SELECT food, calories FROM food WHERE healthy_unhealthy = '$uh' ORDER BY id DESC";
+
+            if($query_run = mysqli_query($con, $query)){
+
+              while ($query_row = mysqli_fetch_assoc($query_run)) {
+
+                $food = $query_row["food"];
+                $calories = $query_row['calories'];
+                echo $query_row['food'] . ' has ' . $calories . ' calories.<br/>';
+
+              }
+
+              mysqli_free_result($query_run);
+              mysqli_close($con);
+
+            }else{
+
+              echo("Error description: " . mysqli_error($con));
+              
+            }
+        }
+    }
+?>
